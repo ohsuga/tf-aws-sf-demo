beeb57579c33fe64629c2e31e9b98e9e85ae4005
ohsuga <ohsuga@gmail.com>
2026-03-17 16:09:45 +0900

    fix duplicated define

--- modules/snowflake/variables.tf
+++ modules/snowflake/variables.tf
@@ -16,17 +16,3 @@ variable "target_directory" {
   default = "landing" # S3内の特定ディレクトリ
 }
 
-variable "storage_aws_role_arn" {
-  type        = string
-  description = "AWS側で作成したIAMロールのARN"
-}
-
-variable "bucket_name" {
-  type = string
-}
-
-variable "target_directory" {
-  type    = string
-  default = "landing" # S3内の特定ディレクトリ
-}
-
