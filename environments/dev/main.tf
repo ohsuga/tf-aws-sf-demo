b5bef9252592c6a890f34b5da59343f019e88ba4
ohsuga <ohsuga@gmail.com>
2026-03-17 16:39:09 +0900

    fix typo

--- environments/dev/main.tf
+++ environments/dev/main.tf
@@ -27,13 +27,13 @@ variable "snowflake_organization_name" {}
 variable "snowflake_user" {}
 
 provider "snowflake" {
-  account_name     = var.snowflake_account_name
-  organizatio_name = var.snowflake_organization_name
-  user             = var.snowflake_user
-  authenticator    = "JWT"
-  private_key      = data.aws_secretsmanager_secret_version.snowflake_key.secret_string
-  alias            = "sys_admin"
-  role             = upper("tf_aws_sf_${var.env}_role")
+  account_name      = var.snowflake_account_name
+  organization_name = var.snowflake_organization_name
+  user              = var.snowflake_user
+  authenticator     = "JWT"
+  private_key       = data.aws_secretsmanager_secret_version.snowflake_key.secret_string
+  alias             = "sys_admin"
+  role              = upper("tf_aws_sf_${var.env}_role")
 }
 
 module "snowflake" {
